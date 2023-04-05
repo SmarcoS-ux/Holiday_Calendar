e8b5cf5530254025410f9414b8546f3f2467533c
F-E-R-A <marcosandreserafim123@gmail.com>
2023-04-05 12:26:23 -0300

    Update layout - MediaQueires

---
+++ holiday_calendar/src/Components/Pages/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+const Footer = () => {
+    return(
+        <div className='Footer'>
+            <p>@CopyRight - Marcos André</p>
+        </div>
+    );
+}
+
+export default Footer;
--- holiday_calendar/src/Components/Pages/Main/index.js
+++ holiday_calendar/src/Components/Pages/Main/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import './style.css';
 
 import Header from '../Header/';
+import Footer from '../Footer/';
 
 const Main = () => {
     return(
@@ -15,9 +16,37 @@ const Main = () => {
                         <h2>Clique no botão abaixo para exibir os feriados Nacionais:</h2>
                         <button>Exibir Feriados</button>
                     </div>
+                    <div className='today-holiday holiday'>
+                        <p id='title_holiday_today'>Este mês tem feriado!</p>
+                        <p><span>Nome do feriado: Teste</span></p>
+                        <p><span>Data: 01/01</span></p>
+                        <div className='border_botton_decoration'></div>
+                    </div>
+                    <div className="next-holidays">
+                        <div className='holiday'>
+                            <p className='title_next_holidays'>Próximo feriado:</p>
+                            <p><span>Nome do feriado: Teste</span></p>
+                            <p><span>Data: 01/01</span></p>
+                            <div className='border_botton_decoration'></div>
+                        </div>
+                        <div className='holiday'>
+                            <p className='title_next_holidays'>Próximo feriado:</p>
+                            <p><span>Nome do feriado: Teste</span></p>
+                            <p><span>Data: 01/01</span></p>
+                            <div className='border_botton_decoration'></div>
+                        </div>
+                        <div className='holiday'>
+                            <p className='title_next_holidays'>Próximo feriado:</p>
+                            <p><span>Nome do feriado: Teste</span></p>
+                            <p><span>Data: 01/01</span></p>
+                            <div className='border_botton_decoration'></div>
+                        </div>
+                    </div>
                 </div>
             </main>
-            
+            <footer className="footer">
+                <Footer/>
+            </footer>
         </div>
     );
 }
